01a36a11002200f96f2d398841d420840a97c06a
gjbenton <gjbenton@dons.usfca.edu>
2020-09-18 13:34:30 -1000

    minDeletionSize is at working functionality... probably could optimize it better

--- SortColumns.java
+++ SortColumns.java
@@ -3,51 +3,69 @@ import java.util.ArrayList;
 
 public class SortColumns{
 	private List<Integer> deletedIndex;
-
+	
 	public SortColumns(){
 		deletedIndex = new ArrayList<Integer>();
-
 	}
+	
 	void add(int n){
 		deletedIndex.add(n);
+	}
+	void get(int n){
+		deletedIndex.get(n);
 	}
 	public String toString(){
 		return deletedIndex.toString();
 	}
-	int size(){
-		return deletedIndex.size();
-	}
-	int get(int num){
-		return deletedIndex.get(num);
-	}
 
 	List<Integer> minDeletionSize(String[] given){
-		//recognizes beginning & middle but not END indices
-		for(int word = 0; word<given.length-1; word++){
-			for(int letter = 0; letter < given.length; letter++){
-				if(given[word].charAt(letter) >= given[word+1].charAt(letter))
-					deletedIndex.add(letter);
+		if(valid(given)){
+			for(int letter = 0; letter<given[0].length(); letter++){
+				for(int word = 0; word < given.length-1; word++){
+					if(given[word].charAt(letter) > given[word+1].charAt(letter)){
+						if (!deletedIndex.contains(letter))
+							deletedIndex.add(letter);
+					}					
+				}
 			}
-		}
-		return deletedIndex;
+			return deletedIndex;
+		}	
+		else
+			deletedIndex.add(-1);
+			return deletedIndex;
+	}
+	boolean valid(String[] given){
+		//compares the size of the elements in the array, eg returns false for avengers
+		for(int i=0; i<given.length-1; i++)
+			if(given[i].length() != given[i+1].length())
+				return false;
+		return true;	
 	}
 
 	public static void main(String[] args){
+		//expected output [1]
 		SortColumns unordered = new SortColumns();
-
-		String[] values = new String[2];
-		values[0]= "efc";
-		values[1]= "abd";
-
-		System.out.println(values);
-		
-		unordered.minDeletionSize(values);
+		String[] ex1 = {"cba", "daf", "ghi"};
+		unordered.minDeletionSize(ex1);
 		System.out.println("Deleted Column Indices: " + unordered);
 
-		// SortColumns ordered = new SortColumns();
-		// ordered.add("121");
-		// ordered.add("542");
-		// ordered.add("963");
+		//expected output []
+		SortColumns inOrder = new SortColumns();
+		String[] ex2 = {"a", "b"};
+		inOrder.minDeletionSize(ex2);
+		System.out.println("Deleted Column Indices: " + inOrder);
+
+		//expected output [0,1,2]
+		SortColumns backOrdered = new SortColumns();
+		String[] ex3 = {"zyx", "wvu", "tsr"};
+		backOrdered.minDeletionSize(ex3);
+		System.out.println("Deleted Column Indices: " + backOrdered);
+		
+		//expected output [-1]
+		SortColumns avengers = new SortColumns();
+		String[] ex4 = {"Captain", "Marvel", "saved", "the", "Avengers"};
+		avengers.minDeletionSize(ex4);
+		System.out.println("Deleted Column Indices: " + avengers);
 		
 	}
 }
